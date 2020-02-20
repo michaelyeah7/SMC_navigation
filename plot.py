3060d990a19ae81cc9ff37261e8fcd2c6e5a3ec7
root <michaelyeah7@126.com>
2020-02-20 22:43:41 +0000

    add plot.py

---
+++ plot.py
@@ -0,0 +1,16 @@
+import numpy as np
+import matplotlib.pyplot as plt
+filename = 'cal.log'
+with open(filename) as f:
+    lines = [float(line.rstrip()) for line in f]
+print(lines)
+averaged_rewards = []
+i = 0
+step = 50
+while  i < len(lines):
+	averaged_reward = np.average(lines[i:i+step])
+	averaged_rewards .append(averaged_reward)
+        i += step
+plt.plot(averaged_rewards)
+plt.savefig('rewards.png')
+
